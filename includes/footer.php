64e5907afe54322f005d8a35bf056f799a0cbb29
qoyri <bastien74430@gmail.com>
2024-07-03 02:52:42 +0200

    last update

--- includes/footer.php
+++ includes/footer.php
@@ -10,18 +10,18 @@
         <div class="row-footer">
 
             <!-- À propos de la section -->
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6-footer col-sm-12">
                 <h5 class="text-uppercase mb-4">À propos</h5>
                 <p class="mb-0">Le musée d'Annecy est un pilier du patrimoine culturel dans la région d'Annecy depuis plus de 50 ans. Nous sommes dédiés à présenter des expositions de qualité supérieure, à inspirer un amour pour l'art et la culture, et à éduquer les visiteurs de tous âges. Notre mission est d'explorer et de célébrer le riche tapestry d'histoire artistique et culturelle d'Annecy.</p>
             </div>
             <!-- Nous contacter la section -->
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6-footer col-sm-12">
                 <h5 class="text-uppercase mb-4">Nous contacter</h5>
                 <p><a href="/contact" class="text-reset">Informations de contact</a></p>
             </div>
 
             <!-- Liens utiles -->
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-6-footer col-sm-12">
                 <h5 class="text-uppercase mb-4">Liens utiles</h5>
                 <ul class="list-unstyled">
                     <li><a href="/contact" class="text-reset">Contact</a></li>
